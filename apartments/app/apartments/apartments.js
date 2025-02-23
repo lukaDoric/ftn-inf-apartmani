4227a53bd0dc53e8261b162664438f9f9cc8c294
Nikola Luburić <luburic91@gmail.com>
2025-02-23 09:12:28 +0100

    refactor: Introduces local storage.

--- apartments/app/apartments/apartments.js
+++ apartments/app/apartments/apartments.js
@@ -1,101 +1,95 @@
 class Apartment {
-    constructor(name, capacity, pricePerNight, petsAllowed) {
-      this.name = name
-      this.capacity = capacity
-      this.pricePerNight = pricePerNight
-      this.petsAllowed = petsAllowed
-    }
+  constructor(name, capacity, pricePerNight, petsAllowed) {
+    this.name = name
+    this.capacity = capacity
+    this.pricePerNight = pricePerNight
+    this.petsAllowed = petsAllowed
   }
+}
   
-  let apartments = []
+let apartments = []
   
-  function createApartmentRow(apartment, index) {
-    let table = document.querySelector("#apartments")
+function createApartmentRows() {
+  let table = document.querySelector("#apartments")
+
+  for (let i = 0; i < apartments.length; i++) {
     let tr = document.createElement("tr")
-  
+    
     let rb = document.createElement("td")
     let name = document.createElement("td")
-  
-    rb.textContent = index + 1
-    name.textContent = apartment.name
-  
+    
+    rb.textContent = i + 1
+    name.textContent = apartments[i].name
+    
     tr.appendChild(rb)
     tr.appendChild(name)
     table.appendChild(tr)
-  
+    
     tr.addEventListener("click", function () {
-      displayApartmentDetails(apartment)
+      displayApartmentDetails(apartments[i])
     })
   }
+}
 
-  function displayApartmentDetails(apartment) {
-    window.location.href = `../apartmentDetails/apartmentDetails.html?name=${apartment.name}&pricePerNight=${apartment.pricePerNight}&capacity=${apartment.capacity}&petsAllowed=${apartment.petsAllowed}`
+function displayApartmentDetails(apartment) { // TODO
+  let petsAllowed = "NO"
+  if (apartment.pets === 'true') petsAllowed = "YES"
+
+  let apartmentName = document.querySelector("#name")
+  apartmentName.innerHTML = apartment.name
+
+  let description = document.querySelector("#description")
+  description.innerHTML = "<br>" + "Capacity: " + apartment.capacity + "<br>" + "Price (per night): " + apartment.pricePerNight +
+  "<br>" + "Pets allowed: " + petsAllowed
+}
+
+
+function initializeApartments() {
+  let apartmentsString = localStorage.getItem("apartments");
+  if(apartmentsString) {
+    apartments = JSON.parse(apartmentsString)
   }
   
-  
-  function initializeApartments() {
+  createApartmentRows()
+  drawChart()
+}
 
-    apartments = [
-      new Apartment("Bella Vista", 2, 37, false),
-      new Apartment("Fontana", 7, 67, true),
-      new Apartment("Crown", 6, 52, false),
-    ]
+function drawChart() {
+  const ctx = document.getElementById("myChart")
+  let labels = []
+  let colors = []
+  let borderColors = []
+  let data = []
 
-    const urlParams = new URLSearchParams(window.location.search)
-    if (urlParams.get("name") != null) {
-        const name = urlParams.get("name")
-        const capacity = urlParams.get("capacity")
-        const pricePerNight = urlParams.get("pricePerNight")
-        const petsAllowed = urlParams.get("petsAllowed")
-        let newApartment = new Apartment(name, capacity, pricePerNight, petsAllowed)
-        apartments.push(newApartment)
-    }
-  
-    for (let i = 0; i < apartments.length; i++) {
-      createApartmentRow(apartments[i], i)
-    }
-  
-    drawChart()
+  for (let i = 0; i < apartments.length; i++) {
+    labels.push(apartments[i].name)
+    colors.push("rgba(99, 133, 255, 0.2)")
+    borderColors.push("rgb(99, 174, 255)")
+    data.push(apartments[i].pricePerNight)
   }
-  
-  function drawChart() {
-    const ctx = document.getElementById("myChart")
-    let labels = []
-    let colors = []
-    let borderColors = []
-    let data = []
-  
-    for (let i = 0; i < apartments.length; i++) {
-      labels.push(apartments[i].name)
-      colors.push("rgba(255, 99, 132, 0.2)")
-      borderColors.push("rgb(255, 99, 132)")
-      data.push(apartments[i].pricePerNight)
-    }
-  
-    new Chart(ctx, {
-      type: "bar",
-      data: {
-        labels: labels,
-        datasets: [
-          {
-            label: "Cene hotela po noci",
-            data: data,
-            backgroundColor: colors,
-            borderColor: borderColors,
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true,
-          },
+
+  new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "Cene hotela po noci",
+          data: data,
+          backgroundColor: colors,
+          borderColor: borderColors,
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
         },
       },
-    })
-  }
-  
-  
-  document.addEventListener("DOMContentLoaded", initializeApartments)
-  
+    },
+  })
+}
+
+document.addEventListener("DOMContentLoaded", initializeApartments)
